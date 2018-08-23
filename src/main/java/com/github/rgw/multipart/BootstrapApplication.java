8e19f3756950a5b08126361b02dfae18d102273f
liangsibin <liangsibin@189.cn>
2018-08-23 10:45:58 +0800

    init

---
+++ src/main/java/com/github/rgw/multipart/BootstrapApplication.java
@@ -0,0 +1,19 @@
+package com.github.rgw.multipart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class BootstrapApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootstrapApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(BootstrapApplication.class);
+	}
+}
